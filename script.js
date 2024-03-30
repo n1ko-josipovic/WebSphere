43e25a8af8341a4e059e1d747d591251b484e669
n1ko-josipovic <144704565+n1ko-josipovic@users.noreply.github.com>
2024-03-30 15:01:23 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+/* For slider */
+var slides = document.querySelectorAll('#slider .slide');
+
+var currentLeftSlide = slides.length-1;
+var currentActiveSlide = 0;
+var currentRightSlide = 1;
+
+function changeSlide(direction) {
+    slides[currentRightSlide].classList.remove('right');
+    slides[currentActiveSlide].classList.remove('active');
+    slides[currentLeftSlide].classList.remove('left');
+
+    currentRightSlide = (currentRightSlide+direction+slides.length) % slides.length;
+    currentActiveSlide = (currentActiveSlide+direction+slides.length) % slides.length;
+    currentLeftSlide = (currentLeftSlide+direction+slides.length) % slides.length;
+
+    slides[currentLeftSlide].classList.add('left');  
+    slides[currentActiveSlide].classList.add('active');
+    slides[currentRightSlide].classList.add('right');
+}
+
+/* For navigation */
 document.addEventListener('DOMContentLoaded', function () {
   const exploreButton = document.querySelector('.b-toggle');
   const hiddenButtons = document.querySelectorAll('.btn.hidden');
@@ -9,15 +31,7 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-var slides = document.querySelectorAll('#slider .slide');
-var currentSlide = 0;
-
-function changeSlide(direction) {
-    slides[currentSlide].classList.remove('active');
-    currentSlide = (currentSlide+direction+slides.length) % slides.length;
-    slides[currentSlide].classList.add('active');
-}
-
+/* For tooltip */
 const h2Elements = document.querySelectorAll('.hoverable');
 const container = document.querySelector('.tooltip-container');
 
